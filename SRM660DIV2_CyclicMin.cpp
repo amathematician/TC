b6f8ae6646d87d38bb2da286edc51672716a3305
amathematician <hangjun.x@gmail.com>
2015-10-20 22:30:53 -0700

    Update SRM660DIV2_CyclicMin.cpp

--- SRM660DIV2_CyclicMin.cpp
+++ SRM660DIV2_CyclicMin.cpp
@@ -89,3 +89,35 @@ Returns: "aaaisgb"
 
 
 This problem statement is the exclusive and proprietary property of TopCoder, Inc. Any unauthorized use or reproduction of this information without the prior written consent of TopCoder, Inc. is strictly prohibited. (c)2003, TopCoder, Inc. All rights reserved. 
+
+
+
+/* Solution */
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+class Cyclemin {
+public:
+	string bestmod(string s, int k) {
+		int n = s.size();
+		string best = s;
+		for (int iter = 1; iter <= n; iter++) {
+			string sCopy = s;
+			int uk = 0;
+			for (int j = 0; j < n; j++) {
+				if (uk < k && s[j] != 'a') {
+					uk++;
+					sCopy[j] = 'a';
+				}
+			}
+			best = min(best, sCopy);
+			s = s.substr(1) + s.substr(0, 1);
+		}
+		return best;
+	}
+};
+
+
